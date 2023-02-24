92636617d589d1899639dac4ac2c35c58516a86f
nr053 <nro@mindfuture.com>
2023-02-24 16:44:26 +0100

    added montage to adf.py script

--- edf_info.py
+++ edf_info.py
@@ -11,6 +11,7 @@ import numpy as np
 import os
 import argparse
 import glob
+from collections import Counter
 
 
 argParser = argparse.ArgumentParser()
@@ -19,22 +20,33 @@ args = argParser.parse_args()
 
 path = '/Users/toucanfirm/Documents/DTU/Speciale/TUSZ_V2/edf/' + args.set
 
-s_freqs = []
-lowpass_list = []
-highpass_list = []
+s_freqs = []      #empty list of sample frequencies
+lowpass_list = []       #empty list of lowpass filters
+highpass_list = []      #empty list of highpass filters
+ch_names_set = set()       #empty set of channel names
 
-for f in glob.glob(path + '/**/*.edf', recursive=True):
-      data = mne.io.read_raw_edf(f)
-        
-      sampling_freq = data.info['sfreq']
-      lowpass = data.info['lowpass']
-      highpass = data.info['highpass']
-        
-      s_freqs.append(sampling_freq)
-      lowpass_list.append(lowpass)
-      highpass_list.append(highpass)
+cnt = Counter()
 
-freq_set = set(s_freqs)
+for f in glob.glob(path + '/**/*.edf', recursive=True):     #iterate through every edf file
+      data = mne.io.read_raw_edf(f, infer_types=True)
+        
+      sampling_freq = data.info['sfreq']        #extract sampling frequency
+      lowpass = data.info['lowpass']            #extract low pass filter
+      highpass = data.info['highpass']          #extract high pass filter
+        
+      s_freqs.append(sampling_freq)       #append sampling frequency to sampling freq list
+      lowpass_list.append(lowpass)        #append low pass filter to list
+      highpass_list.append(highpass)      #append high pass filter to list
+
+      for chName in data.info['ch_names']:
+            chName = chName.removesuffix("-REF")
+            chName = chName.removesuffix("-LE")
+            ch_names_set.add(chName)
+            cnt[chName] += 1
+
+
+
+freq_set = set(s_freqs)       #make set from list to remove duplicates
 lowpass_set = set(lowpass_list)
 highpass_set = set(highpass_list)
 
@@ -54,3 +66,5 @@ for filter in highpass_set:
 print("Sampling frequencies: ", sfreq_counts)
 print("Low pass filters : ", lowpass_counts)
 print("High pass filters: ", highpass_counts)
+print(cnt)
+print(len(cnt))
